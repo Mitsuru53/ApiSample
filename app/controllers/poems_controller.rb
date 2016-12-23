bd088e4860b21644738a3d5d5400390c2978ee2d
Mitsuru Hashimoto <exhikkii@gmail.com>
2016-12-23 17:15:52 +0900

    cazzo

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,6 +1,22 @@
 class PoemsController < ApplicationController
+  before_action :set_poem, only: [:show]
+
   def index
     @poems = Poem.all
     render json: @poems
+  end
+
+  def show
+    # @poem = Poem(poems_params)
+    render json: @poem
+  end
+
+  private
+  def poems_params
+    params.require(:poem).permit(:title, :content)
+  end
+
+  def set_poem
+    @poem = Poem.find(params[:id])
   end
 end
